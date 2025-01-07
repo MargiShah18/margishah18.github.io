d9f876e8eb6ab196f3cfacb0b4e45a5352de2b94
Margi Shah <margishah1833@gmail.com>
2025-01-06 20:31:59 -0500

    removed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { remarkReadingTime } from "./remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://margishah18.github.io",
-  base: "/portfolio",
   output: "static",
   integrations: [tailwind(), icon()],
   markdown: {
