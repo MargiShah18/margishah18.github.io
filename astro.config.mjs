4d51318cb5b3a173ebb1e743eadf9417157210bf
Margi Shah <margishah1833@gmail.com>
2025-01-06 19:39:09 -0500

    Configure GitHub Actions for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://MargiShah18.github.io",
+  base: "/",
+  output: "static",
   integrations: [tailwind(), icon()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
